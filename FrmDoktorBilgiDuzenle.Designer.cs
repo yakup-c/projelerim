f0e3f19fb9fbb53863ee4cf1d57944417b3b3487
Yakup Çil <66686561+yakup-c@users.noreply.github.com>
2023-09-25 19:02:48 +0300

    Add files via upload

---
+++ FrmDoktorBilgiDuzenle.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Hastane_Otomysn_Proje
+{
+    partial class FrmDoktorBilgiDuzenle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDoktorBilgiDuzenle));
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.MskTC = new System.Windows.Forms.MaskedTextBox();
+            this.LblSoyad = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.LblSifre = new System.Windows.Forms.Label();
+            this.CmbBrans = new System.Windows.Forms.ComboBox();
+            this.LblBrans = new System.Windows.Forms.Label();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtAd.Location = new System.Drawing.Point(198, 69);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(167, 33);
+            this.TxtAd.TabIndex = 33;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(138, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 26);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Ad:";
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSoyad.Location = new System.Drawing.Point(198, 109);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(167, 33);
+            this.TxtSoyad.TabIndex = 31;
+            // 
+            // MskTC
+            // 
+            this.MskTC.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskTC.Location = new System.Drawing.Point(198, 148);
+            this.MskTC.Mask = "00000000000";
+            this.MskTC.Name = "MskTC";
+            this.MskTC.Size = new System.Drawing.Size(167, 33);
+            this.MskTC.TabIndex = 30;
+            this.MskTC.ValidatingType = typeof(int);
+            // 
+            // LblSoyad
+            // 
+            this.LblSoyad.AutoSize = true;
+            this.LblSoyad.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblSoyad.Location = new System.Drawing.Point(108, 112);
+            this.LblSoyad.Name = "LblSoyad";
+            this.LblSoyad.Size = new System.Drawing.Size(73, 26);
+            this.LblSoyad.TabIndex = 29;
+            this.LblSoyad.Text = "Soyad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(50, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 26);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "TC Kimlik No:";
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSifre.Location = new System.Drawing.Point(198, 235);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(167, 33);
+            this.TxtSifre.TabIndex = 35;
+            // 
+            // LblSifre
+            // 
+            this.LblSifre.AutoSize = true;
+            this.LblSifre.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblSifre.Location = new System.Drawing.Point(120, 238);
+            this.LblSifre.Name = "LblSifre";
+            this.LblSifre.Size = new System.Drawing.Size(61, 26);
+            this.LblSifre.TabIndex = 34;
+            this.LblSifre.Text = "Şifre:";
+            // 
+            // CmbBrans
+            // 
+            this.CmbBrans.FormattingEnabled = true;
+            this.CmbBrans.Location = new System.Drawing.Point(198, 195);
+            this.CmbBrans.Name = "CmbBrans";
+            this.CmbBrans.Size = new System.Drawing.Size(167, 34);
+            this.CmbBrans.TabIndex = 37;
+            // 
+            // LblBrans
+            // 
+            this.LblBrans.AutoSize = true;
+            this.LblBrans.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblBrans.Location = new System.Drawing.Point(108, 195);
+            this.LblBrans.Name = "LblBrans";
+            this.LblBrans.Size = new System.Drawing.Size(70, 26);
+            this.LblBrans.TabIndex = 38;
+            this.LblBrans.Text = "Branş:";
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.BackColor = System.Drawing.Color.Bisque;
+            this.BtnGuncelle.Location = new System.Drawing.Point(198, 287);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(167, 61);
+            this.BtnGuncelle.TabIndex = 39;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = false;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // FrmDoktorBilgiDuzenle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(435, 427);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.LblBrans);
+            this.Controls.Add(this.CmbBrans);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.LblSifre);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.MskTC);
+            this.Controls.Add(this.LblSoyad);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Corbel", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "FrmDoktorBilgiDuzenle";
+            this.Text = "Doktor Bilgi Güncelle";
+            this.Load += new System.EventHandler(this.FrmDoktorBilgiDuzenle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.MaskedTextBox MskTC;
+        private System.Windows.Forms.Label LblSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Label LblSifre;
+        private System.Windows.Forms.ComboBox CmbBrans;
+        private System.Windows.Forms.Label LblBrans;
+        private System.Windows.Forms.Button BtnGuncelle;
+    }
+}
